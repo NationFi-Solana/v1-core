0c2c968ac07a54622a1ccffc003ccaaa0c7c3320
Travis <travistarpley0@gmail.com>
2024-06-10 14:37:58 -0700

    feat: adding function

--- web/components/bet/use-bet-program.ts
+++ web/components/bet/use-bet-program.ts
@@ -1,11 +1,13 @@
 'use client';
-
 import { useConnection } from '@solana/wallet-adapter-react';
 import { useCluster } from '../cluster/cluster-data-access';
 import { useMemo } from 'react';
 import { getBettingProgram, getBettingProgramId } from '@test/anchor';
 import { useAnchorProvider } from '../solana/solana-provider';
 import { useMutation } from '@tanstack/react-query';
+import { Keypair, PublicKey } from '@solana/web3.js';
+import { Address, BN } from '@coral-xyz/anchor';
+import { findProgramAddressSync } from '@project-serum/anchor/dist/cjs/utils/pubkey';
 
 export function useBetProgram() {
   const { connection } = useConnection();
@@ -18,10 +20,24 @@ export function useBetProgram() {
     mutationKey: ['a'],
     mutationFn: (keypair: Keypair) =>
       program.methods
-        .placeSplBet()
-        .accounts({ programAuthority: '' })
+        .placeSplBet(0, new BN(1000), 9)
+        .accounts({
+          programStateAccount: deriveProgramStateAccountPda(programId),
+          userSplBalance: '',
+          userTokenAccount: '',
+          splTokenMint: '',
+          programTokenAccountForSpl: '',
+          programAuthority: ''
+        })
         .signers([keypair])
         .rpc(),
   });
   return {};
+}
+
+
+function deriveProgramStateAccountPda(programId: PublicKey) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [pda] = findProgramAddressSync([Buffer.from('state')], programId)
+  return pda;
 }
