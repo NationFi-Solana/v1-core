648dd29c4474e783b7ff90928a97716ac4f30cab
Travis <travistarpley0@gmail.com>
2024-06-10 15:11:32 -0700

    feat: Accounts retrieval for place bet

--- web/components/bet/use-bet-program.ts
+++ web/components/bet/use-bet-program.ts
@@ -1,38 +1,47 @@
 'use client';
-import { useConnection } from '@solana/wallet-adapter-react';
-import { useCluster } from '../cluster/cluster-data-access';
+import { useAnchorWallet } from '@solana/wallet-adapter-react';
+
 import { useMemo } from 'react';
 import { getBettingProgram, getBettingProgramId } from '@test/anchor';
 import { useAnchorProvider } from '../solana/solana-provider';
 import { useMutation } from '@tanstack/react-query';
 import { Keypair, PublicKey } from '@solana/web3.js';
-import { Address, BN } from '@coral-xyz/anchor';
+import { BN } from '@coral-xyz/anchor';
 import { findProgramAddressSync } from '@project-serum/anchor/dist/cjs/utils/pubkey';
+import { token } from '@coral-xyz/anchor/dist/cjs/utils';
 
 export function useBetProgram() {
-  const { connection } = useConnection();
-  const { cluster } = useCluster();
+
+  const wallet = useAnchorWallet()
   const provider = useAnchorProvider();
   const programId = useMemo(() => getBettingProgramId(), []);
   const program = getBettingProgram(provider, '');
-
+  const mint = new PublicKey('3yZEgJVK41MvLuWKvHh6bpaLwqynaQG7BYwB4bPdCnFj')
+  const tokenAccountForMint = new PublicKey('C5r3YfGioRAziHonypJDpMG9qa94yLSN8qXwNjmcjwTs')
   const placeBet = useMutation({
     mutationKey: ['a'],
-    mutationFn: (keypair: Keypair) =>
-      program.methods
+    mutationFn: (keypair: Keypair) => {
+      const ownerTokenAddress = token.associatedAddress({
+        mint,
+        owner: programId,
+      });
+      const [userSplBalancePda] = findProgramAddressSync([tokenAccountForMint.toBuffer(), Buffer.from('spl_bet_b')], programId)
+      return program.methods
         .placeSplBet(0, new BN(1000), 9)
         .accounts({
           programStateAccount: deriveProgramStateAccountPda(programId),
-          userSplBalance: '',
-          userTokenAccount: '',
-          splTokenMint: '',
-          programTokenAccountForSpl: '',
-          programAuthority: ''
+          userSplBalance: userSplBalancePda,
+          userTokenAccount: tokenAccountForMint,
+          splTokenMint: mint,
+          programTokenAccountForSpl: ownerTokenAddress,
+          userAuthority: wallet?.publicKey ?? '',
+          programAuthority: programId
         })
         .signers([keypair])
-        .rpc(),
+        .rpc()
+    }
   });
-  return {};
+  return { placeBet };
 }
 
 
