616c00a49d99426b10215546a54522209e7abdbe
Travis <travistarpley0@gmail.com>
2024-06-12 10:41:52 -0700

    feat: check if user can submit buy form

--- web/components/bet/hooks/isSubmitValid.ts
+++ web/components/bet/hooks/isSubmitValid.ts
@@ -1,4 +1,16 @@
-export const useSubmitValid = () => {
 
+interface Props {
+    deposit: string
+    balance: number | undefined
+}
+export const useSubmitValid = ({ deposit, balance }: Props) => {
+    const amount = parseFloat(deposit)
+    if (!balance) {
+        return { isValid: false, errorMessage: 'Failed to fetch balance.' }
+    }
+    const bal = balance / 10 ** 9
+    if (amount > bal) {
+        return { isValid: false, errorMessage: "Insufficent balance." }
+    }
     return { isValid: true }
 }
