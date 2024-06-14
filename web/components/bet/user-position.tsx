80d55d67ccf9e509d5cc2c4425b49b2f8795fbed
Travis <travistarpley0@gmail.com>
2024-06-14 12:24:01 -0700

    feat: user position styles

--- web/components/bet/user-position.tsx
+++ web/components/bet/user-position.tsx
@@ -4,6 +4,7 @@ import { Button } from '../shared/ui/button';
 import { useGetUserPosition } from './hooks/get-user-position';
 import { useCancelBet } from './hooks/use-bet-program';
 import BetProgressAlert from './bet-progress-alert';
+import { SiSolana } from 'react-icons/si';
 
 export function UserPosition({
   option,
@@ -22,10 +23,13 @@ export function UserPosition({
   return (
     <form onSubmit={onSubmit}>
       <BetProgressAlert waitForSign={cashOut.isPending} isTxPending={false} />
-      <div className="flex border-b border-gray-600 pb-1 items-center  justify-between">
+      <div className="flex border-b border-gray-600 pb-2 items-center  justify-between">
         <h1 className="font-archivo ">{option}</h1>
         <div className="flex items-center gap-x-2">
-          <h1 className="">{data?.balance.toString() ?? '0'} SOL</h1>
+          <h1 className="flex items-center gap-x-2 pr-4">
+            {data?.balance.toString() ?? '0'}{' '}
+            <SiSolana size={15} className="text-primary" />
+          </h1>
 
           <Button type="submit" disabled={!data || !bal0} variant="destructive">
             Withdraw
