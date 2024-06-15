314b3f47626321a35fa8d370fd63fc5d4119e248
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-06-14 21:45:00 -0700

    feat: querykey

--- web/components/bet/hooks/get-user-position.ts
+++ web/components/bet/hooks/get-user-position.ts
@@ -1,30 +1,38 @@
-import { useGetBetProgram } from "@/components/shared/hooks/get-bet-program";
-import { useQuery } from "@tanstack/react-query";
-import { getSolPDA } from "./use-bet-program";
-import { useWallet } from "@solana/wallet-adapter-react";
+import { useGetBetProgram } from '@/components/shared/hooks/get-bet-program';
+import { useQuery } from '@tanstack/react-query';
+import { getSolPDA } from './use-bet-program';
+import { useWallet } from '@solana/wallet-adapter-react';
 
 export function useGetUserPosition({ isBetA }: { isBetA: boolean }) {
+  const { program, programId } = useGetBetProgram();
+  const wallet = useWallet();
+  const { userSolBalancePda } = getSolPDA({
+    isBetA,
+    user: wallet.publicKey,
+    programId,
+  });
+  const { data } = useQuery({
+    queryKey: [
+      'userPosition',
+      wallet.publicKey,
+      programId.toString(),
+      isBetA ? '0' : '1',
+    ],
+    queryFn: async () => {
+      if (wallet.publicKey !== null) {
+        const balance = await program.account.signerSolBalance.fetch(
+          userSolBalancePda
+        );
+        console.log(balance, 'BALANCE');
+        return balance;
+      } else {
+        console.log('ERROR');
+        throw new Error('Not connected');
+      }
+    },
+  });
 
-    const { program, programId } = useGetBetProgram()
-    const wallet = useWallet()
-    const { userSolBalancePda } = getSolPDA({ isBetA, user: wallet.publicKey, programId })
-    const { data } = useQuery({
-        queryKey: ['userPosition', wallet.publicKey, programId.toString(), isBetA ? '0' : '1'],
-        queryFn: async () => {
-            if (wallet.publicKey !== null) {
-                const balance = await program.account.signerSolBalance.fetch(
-                    userSolBalancePda
-                );
-                console.log(balance, 'BALANCE');
-                return balance;
-            } else {
-                console.log('ERROR');
-                throw new Error('Not connected');
-            }
-        },
-    });
-
-    return {
-        data
-    }
+  return {
+    data,
+  };
 }
