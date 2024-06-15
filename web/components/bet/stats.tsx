7753acdf4eccedaa21a4a1e1389e60876ad6bcaf
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-06-15 09:52:42 -0700

    styles

--- web/components/bet/stats.tsx
+++ web/components/bet/stats.tsx
@@ -7,7 +7,7 @@ export default function Stats() {
   return (
     <div className="space-y-3 text-sm pt-2">
       <div className="flex justify-between">
-        <h3 className="text-gray-400">Market Cap</h3>
+        <h3 className="text-gray-300">Market Cap</h3>
         <h3>{(a + b) / 10 ** 9}</h3>
       </div>
     </div>
