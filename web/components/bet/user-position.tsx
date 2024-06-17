b2b2fb65af519440846d75a0236264e38da926f8
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-06-16 21:36:10 -0700

    rm withdraw

--- web/components/bet/user-position.tsx
+++ web/components/bet/user-position.tsx
@@ -34,7 +34,7 @@ export function UserPosition({
             <SiSolana size={15} className="text-primary" />
           </h1>
 
-          {programData?.betOver == 0 && programData.betsClosed == 0 && (
+          {/* {programData?.betOver == 0 && programData.betsClosed == 0 && (
             <Button
               type="submit"
               disabled={!data || !bal0}
@@ -42,7 +42,7 @@ export function UserPosition({
             >
               Withdraw
             </Button>
-          )}
+          )} */}
         </div>
       </div>
     </form>
