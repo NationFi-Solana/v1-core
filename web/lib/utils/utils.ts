1bc169b032a904f59e13add56d4cbf6ce43e8a38
Travis <travistarpley0@gmail.com>
2024-06-19 12:23:07 -0700

    feat: rounddown

--- web/lib/utils/utils.ts
+++ web/lib/utils/utils.ts
@@ -13,7 +13,7 @@ export function formatUnixTimestamp(unixTimestamp: number) {
   });
 }
 export function formatDecimal(num: number) {
-  return parseFloat(num.toFixed(5).toString())
+  return parseFloat(Math.floor((num * 1000) / 1000).toFixed(5).toString())
 }
 export function convertToU8Array(num: number) {
   const a = new Uint16Array([num])
