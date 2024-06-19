325fa710a9ff5ce0c0234b0bf2e81a11ff08d550
Travis <travistarpley0@gmail.com>
2024-06-19 14:16:17 -0700

    feat: footer styles

--- web/components/shared/ui/footer.tsx
+++ web/components/shared/ui/footer.tsx
@@ -1,12 +1,26 @@
 import logo from '@/public/nation-logo.webp';
 import Image from 'next/image';
+import { SiDiscord, SiTwitter } from 'react-icons/si';
 export function Footer() {
   return (
-    <footer className="  py-10  px-4 border-t-2 border-gray-500">
-      <div className="w-full flex-grow max-w-[1200px] mx-auto ">
-        <div className="flex items-center gap-x-4">
-          <Image src={logo} width={100} height={100} alt="Logo" />
-          <h2 className="font-archivo text-2xl">NATIONFI</h2>
+    <footer className="  py-10  px-4 border-t-2 border-background-700">
+      <div className="w-full flex-grow  max-w-[1200px] mx-auto ">
+        <div className="flex gap-x-10">
+          <div className="flex items-center gap-x-2 ">
+            <Image src={logo} width={80} height={80} alt="Logo" />
+            <h2 className="font-archivo text-2xl">NationFI</h2>
+          </div>
+
+          <div>
+            <div className="flex flex-col gap-y-2">
+              <div className="bg-background-700 p-2 rounded-md">
+                <SiDiscord size={20} />
+              </div>
+              <div className="bg-background-700 p-2 rounded-md">
+                <SiTwitter size={20}></SiTwitter>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
