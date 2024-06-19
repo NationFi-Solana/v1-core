cd2aa46e9bb41799b1af4677b04c99ce703e6a94
Travis <travistarpley0@gmail.com>
2024-06-19 12:13:20 -0700

    feat: show fee

--- web/components/bet/stats.tsx
+++ web/components/bet/stats.tsx
@@ -7,6 +7,10 @@ export default function Stats() {
   return (
     <div className="space-y-3 text-sm pt-2">
       <div className="flex justify-between">
+        <h3 className="text-gray-300">Fee</h3>
+        <h3 className="text-red-400">0.05%</h3>
+      </div>
+      <div className="flex justify-between">
         <h3 className="text-gray-300">Market Cap</h3>
         <h3>{(a + b) / 10 ** 9}</h3>
       </div>
