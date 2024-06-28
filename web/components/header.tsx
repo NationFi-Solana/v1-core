b767a00eff7417cbcb7a217446b65bcf629c6715
Tarp <travistarpley0@gmail.com>
2024-06-28 14:19:36 -0700

    fix: rm clusters in header

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 
 import logo from '@/public/nation-logo.webp';
 import Image from 'next/image';
-import { ClusterUiSelect } from './cluster/cluster-ui';
 // import { ClusterUiSelect } from './cluster/cluster-ui';
+
 export default function Header() {
   return (
     <div className="flex relative justify-between py-4 bg-transparent text-neutral-content  space-y-2 md:space-y-0  lg:px-10">
@@ -16,20 +16,20 @@ export default function Header() {
           href="/"
         >
           {/* // eslint-disable-next-line @next/next/no-img-element */}
-          <h2 className=" font-archivo text-sm lg:text-lg md:block hidden">
-            NATIONFI Voting
+          <h2 className="font-archivo  text-sm lg:text-lg md:block hidden">
+            NationFi Betting
           </h2>
-          <h2 className=" font-archivo text-sm md:hidden">NV</h2>
+          <h2 className=" font-archivo text-sm md:hidden">NationFi</h2>
           <Image src={logo} className="h-14 w-14" alt="Logo" />
         </Link>
       </div>
-      <div className="flex space-x-4 ">
+      <div className="flex space-x-4   ">
         <div className="flex items-center gap-x-2 text-[0.8rem]">
           {/* <p>Dark Mode</p>
           <Switch onCheckedChange={setIsDarkTheme} /> */}
         </div>
         <WalletButton className="bg-primary" style={{ color: '#e6f9af' }} />
-        <ClusterUiSelect />
+        {/* <ClusterUiSelect /> */}
       </div>
     </div>
   );
